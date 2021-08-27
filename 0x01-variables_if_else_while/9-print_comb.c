7ebf7000c5d2817f86824f9c02046bca75ecfee0
YesseJr <thonnieyesse@hotmail.com>
2021-08-27 16:12:37 +0300

    Print possible combs of 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print all possible combination of numbers separated with commas, using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
