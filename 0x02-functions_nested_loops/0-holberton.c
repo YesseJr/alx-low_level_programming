81e16a376c093ca65352a21959cf5c23f6a98eab
YesseJr <thonnieyesse@hotmail.com>
2021-08-31 12:36:50 +0300

    Fixed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *c = "Holberton";
-	int i;
-	for (i=0; i<10; i++)
-	{
-	_putchar('n');
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
