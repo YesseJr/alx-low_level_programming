357bec2cd7ed2a13b166cd5d1e67e12d5e667778
Waython Yesse <thonnieyesse@hotmail.com>
2021-09-02 13:22:11 +0300

    mainn

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
