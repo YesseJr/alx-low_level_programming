652f33995c8680ffff8273dc32130049baf7cacc
Waython Yesse <thonnieyesse@hotmail.com>
2021-09-23 19:07:23 +0300

    Fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,93 +1,161 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+unsigned int num_len(char *s);
+
+void *_calloc0(unsigned int nmemb, unsigned int size);
+
+void _mul(char *total, char *num1, unsigned int len1, char digit2);
 
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
-
-void _puts(char *str)
-{
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-
-}
-
-/**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * num_len - Returns the length of a string containing numbers. If there's a
+ * non digit, prints Error and exits with status 98.
+ *
+ * @s: A pointer to the character string.
+ *
+ * Return: The length of the character string.
  */
 
-int _atoi(const char *s)
+unsigned int num_len(char *s)
 {
-    int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+	unsigned int length;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	length = 0;
+
+	while (s[length])
 	{
-		if (s[firstNum] == '-')
+		if (s[length] < '0' || s[length] > '9')
 		{
-			sign *= -1;
+			printf("Error\n");
+			exit(98);
 		}
+		length++;
 	}
+	return (length);
+}
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+/**
+  * _calloc0 - Allocates memory for an array using malloc and then sets the
+  * memory to character 0.
+  * @nmemb: Number of elements to allocate for the array.
+  * @size: Size in bytes of each element.
+  *
+  * Return: A void pointer to the first byte of the allocated memory, or NULL.
+  */
+
+void *_calloc0(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+	unsigned int bytes;
+
+	bytes = nmemb * size;
+
+	if (bytes <= 0)
+		return (NULL);
+
+	ptr = malloc(bytes);
+
+	if (!ptr)
+		return (NULL);
+
+	for (i = 0; i < bytes; i++)
+		ptr[i] = '0';
+
+	return ((void *) ptr);
+}
+
+/**
+  * _mul - Iterate through the first number and multiply each digit with the
+  * digit from the second number, then add to total so far.
+  * @total: Pointer to the product of the first number and the second number.
+  * Points specifically to the last byte in memory that will be changed at the
+  * beginning of the function call. During the first function call the pointer
+  * points to the second to last byte in memory (last being the null byte).
+  * Each subsequent time the function is called, the pointer starts 1
+  * byte further to the left than the previous call. The pointer moves left
+  * with each loop during the function execution.
+  * @num1: Pointer to char string containing the first number.
+  * @len1: Length of the first number.
+  * @digit2: Current digit from the second number.
+  *
+  * Return: void.
+  */
+
+void _mul(char *total, char *num1, unsigned int len1, char digit2)
+{
+	int j;
+	unsigned int sum;
+	unsigned int leftover;
+
+	leftover = 0;
+
+	for (j = len1 - 1; j >= 0; j--)
 	{
-		resp *= 10;
-		resp += (s[i] - 48);
+		sum = (num1[j] - '0') * (digit2 - '0') + (*total - '0') + leftover;
+		leftover = sum / 10;
+		*total = (sum % 10) + '0';
+		total--;
+	}
+	*total = leftover + '0';
+}
+
+/**
+  * main - Multiplies two positive numbers and prints to terminal.
+  * @argc: The number of arguments passed.
+  * @argv: A pointer to an array of pointers that point to the arguments.
+  *
+  * Return: 0 upon successful run.
+  */
+
+int main(int argc, char **argv)
+{
+	char *total;
+	char *num1;
+	char *num2;
+	int i;
+	unsigned int len1;
+	unsigned int len2;
+	unsigned int shift_left;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
 
-	return (sign * resp);
-}
+	len1 = num_len(argv[1]);
+	len2 = num_len(argv[2]);
+	num1 = argv[1];
+	num2 = argv[2];
 
-/**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
- */
+	total = _calloc0(len2 + len1 + 1, sizeof(*total));
 
-void print_int(unsigned long int n)
-{
+	if (!total)
+	{
+		printf("Allocation of total has failed\n");
+		exit(98);
+	}
 
-unsigned  long int divisor = 1, i, resp;
+	shift_left = 0;
+	/* iterate through each digit in num2 starting with last*/
+	for (i = len2 - 1; i >= 0; i--)
+	{
+		/* pass address of the 2nd to last byte in memory minus number */
+		/* of bytes that wont change during calculation */
+		_mul((total + len2 + len1 - 1 - shift_left), num1, len1, num2[i]);
+		shift_left++;
+	}
+	total[len2 + len1] = '\0';
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
+	for (i = 0; total[i] == '0'; i++)
+		;
+	if (total[i] != '\0')
+		printf("%s\n", &total[i]);
+	else
+		printf("0\n");
 
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
+	free(total);
 
-}
-
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-(void)argc;
-
-if (argc != 3)
-{
-	_puts("Error ");
-	exit(98);
-}
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
-
-return (0);
+	return (0);
 }
