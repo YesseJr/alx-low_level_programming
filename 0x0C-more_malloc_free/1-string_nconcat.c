d48aca934e757dee757436f50893089326972a74
Waython Yesse <thonnieyesse@hotmail.com>
2021-09-24 05:24:42 +0300

    This one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
-
-/**
- *_strlen - calculate and return string length
- *@string: string
- *Return: string length
- */
-
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
+#include <stdlib.h>
 
 /**
- *string_nconcat - concatenates two strings
- *@s1: string 1
- *@s2: string 2
- *@n: n bytes to concat from string 2
- *Return: pointer to concatenated string
- */
-
+  * string_nconcat - concatenates two strings
+  * @s1: first string to copy
+  * @s2: second string to copy
+  * @n: number of bytes of s2 to copy
+  *
+  * Return: pointer to new char
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int num, len, i, j;
+	char *str;
 
-	num  = n;
+	unsigned int i, j, len = n;
 
-	if (s1 == NULL) /* account for null strings*/
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0) /* account for negative n bytes*/
+
+	for (i = 0; s1[i]; i++)
+		;
+
+	for (j = 0; s2[j]; j++)
+		;
+
+	if (j > n)
+		j = n;
+
+	str = malloc(sizeof(char) * (i + j + 1));
+
+	if (str == NULL)
 		return (NULL);
-	if (num >= _strlen(s2)) /*account for n too big*/
-		num = _strlen(s2);
 
-	len = _strlen(s1) + num + 1; /* +1 for null pointer*/
+	for (len = 0; len < i; len++)
+	{
+		str[len] = s1[len];
+	}
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error*/
-	if (ptr == NULL)
-		return (NULL);
+	for (len = 0; len < j; len++)
+	{
+		str[len + i] = s2[len];
+	}
 
-	for (i = 0; s1[i] != '\0'; i++) /*concat*/
-		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+	str[i + j] = '\0';
 
-	return (ptr);
+	return (str);
 }
