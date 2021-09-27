7ebba221af24a213f71ef5a7d9492fabce9b7830
Waython Yesse <thonnieyesse@hotmail.com>
2021-09-27 21:46:25 +0300

    Hold it

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,127 +1,72 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: num of args
- * @argv: pointer to args
- * Return: int status
- */
-int main(int argc, char *argv[])
+* main - multiplies two positive numbers.
+* @argc: number of arguments
+* @argv: the values in each argc element
+*
+* Description: its like infinite add but with multiplication
+* Return: print the result. return 0 if success
+*/
+
+# IF EITHER NUMBER ARE ZERO RETURN 0;
+##SHOULD CHECK IF BEGINNING LEADING NUMBER IS ZERO. 
+
+
+int main(int argc, char **argv)
 {
-	unsigned int n1, n2, result;
+	int size1 = 0;
+	int size2 = 0;
+	int product_size = 0;
+	int product = 0;
+	int overflow = 0;
+	int index = 0;
+	int tab = 0;
 
-	int j = 1;
-
-	if (argc != 3)
+	while (argv[1][size1])//strlength and checks if digit
 	{
-		print_string("Error in args\n");
-		exit(98);
+		ISDIGIT\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\##################
+		size1++;
 	}
-
-	while (j < argc)
+	while (argv[2][size2])//strlength and checks if digit
 	{
-		if (only_nums(argv[j]) == 0)
+		ISITADIGIT########################################################
+		size2++;
+	}
+	product_size = size1 + size2 + 1;//calculate the range we need
+	product = malloc(sizeof(char) * product_size);//allocates space
+	if (!product)
+		return (98);
+	while (product_size >= 0)//initialize all to 0
+		product[product_size] = '0';
+	size1--;//because length is 1 more than index
+	size2--;
+	while (size2 >= 0)
+	{
+		index = tab;
+		for (j = size1; j >= 0; j--)
 		{
-			print_string("Error in digits\n");
-			exit(98);
+			product = (argv[1][j]-'0') * (argv[2][size2]-'0');
+			product += product[index] -'0';
+			product += overflow;
+			overflow = product / 10;
+			product[index] = product % 10 + '0';
+			index++;
 		}
-		j++;
+		if (overflow)
+		{
+			product[index] = overflow;
+			index++;
+		}
+		overflow = 0;
+		size2--;
+		tab++;
 	}
-
-	n1 = _atoi(argv[1]);
-	n2 = _atoi(argv[2]);
-	result = n1 * n2;
-	print_int(result);
-	_putchar('\n');
+	product[index] ='\0';
+	reversestring\\\\\###########################################################
+	printf("%s\n", product);
+	free(product);
 	return (0);
-}
-/**
- * only_nums - checks for non-digit symbols
- * @s: string
- * Return: 1 if test passed 0 otherwise
- */
-int only_nums(char *s)
-{
-	int c = 0;
-
-	while (*(s + c) != 0)
-	{
-		if (*(s + c) < 48 || *(s + c) > 57)
-		{
-			return (0);
-		}
-		c++;
-	}
-	if (c == 0)
-		return (0);
-	return (1);
-}
-/**
- * _atoi - convert string to int
- * Return: the int
- * @s: string
- */
-int _atoi(char *s)
-{
-	int size = 0, i, j, sign = 1;
-
-	int number = 0;
-
-	while (s[size] != 0)
-	{
-		size++;
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (s[i] >= 48 && s[i] <= 57)
-		{
-			int pow = 1;
-
-			j = i;
-			while (s[j] >= 48 && s[j] <= 57)
-			{
-				j++;
-			}
-			j--;
-			while (j >= i)
-			{
-				number += (s[j] - '0') * pow;
-				pow *= 10;
-				j--;
-			}
-			break;
-		}
-		else if (s[i] == 45)
-			sign = -sign;
-	}
-	if (sign < 0)
-		number = -number;
-	return (number);
-}
-/**
- * print_string - prints string with putchar
- * @s: string
- * Return: void
- */
-void print_string(char s[])
-{
-	int c = 0;
-
-	while (s[c] != 0)
-	{
-		_putchar(s[c]);
-		c++;
-	}
-}
-/**
- * print_int - prints integer
- * @n: integer to print
- */
-void print_int(unsigned int n)
-{
-	if (n / 10)
-		print_int(n / 10);
-	_putchar(n % 10 + '0');
 }
