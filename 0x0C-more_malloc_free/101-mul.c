cca1a55a0f6a010db409f010f82f443c65e92ba6
Waython Yesse <thonnieyesse@hotmail.com>
2021-09-30 05:26:09 +0300

    Mul mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,103 +1,198 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <stddef>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _prt - print string followed by newline
+ * @s: string to print
+ */
+void _prt(char *s)
+{
+	while (*s != '\0')
+		_putchar(*s++);
+	_putchar('\n');
+}
+/**
+ * _realloc - Re-allocate memory for a larger or smaller size
+ * @ptr: Pointer to the old memory block
+ * @old_size: The old size of the memory block
+ * @new_size: The new size of the memory block being created
+ *
+ * Return: Pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *space;
+	char *spacecpy, *ptrcpy;
+	unsigned int i;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	/* regardless, we need to make new space of new_size */
+	space = malloc(new_size);
+	if (space == NULL)
+		return (NULL);
+	/* if ptr is null, return space without copying */
+	if (ptr == NULL)
+		return (space);
+	/* copy old contents into new space */
+	spacecpy = space;
+	ptrcpy = ptr;
+	for (i = 0; i < old_size && i < new_size; i++)
+		spacecpy[i] = ptrcpy[i];
+	free(ptr);
+	return (space);
+}
+/**
+ * _calloc - Allocate memory and initalize space to zero
+ * @nmemb: number of elements
+ * @size: size of bytes
+ *
+ * Return: pointer to memory space, or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *space;
+	char *memset;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	memset = space;
+	for (i = 0 ; i < nmemb * size; i++)
+	{
+		*(memset + i) = 0;
+	}
+
+	return (space);
+}
+/**
+ * _notdigit - check to see if string is only digits
+ * @s: string to check
+ *
+ * Return: 0 if only digits, 1 if non digit chars
+ */
+int _notdigit(char *s)
+{
+	for ( ; *s; s++)
+		if (*s < '0' || *s > '9')
+			return (1);
+	return (0);
+}
+/**
+ * rev_ - Reverse a string in place
+ * @s: string to reverse
+ */
+void rev_(char *s)
+{
+	char tmp;
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+		;
+	i--;
+	for (j = 0; j <= i / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - j];
+		s[i - j] = tmp;
+	}
+}
+/**
+ * _addup - add up integer array
+ * @arr: array to count
+ * @n: number of ints to count
+ * @place: which tens place to count
+ *
+ * Return: result of addition
+ */
+int _addup(int *arr, int n, int place)
+{
+	int sum, i;
+
+	for (i = 0, sum = 0; i < n; i++)
+	{
+		sum += arr[n * i + place];
+	}
+	return (sum);
+}
+/**
+ * cut_zeros - cut off my zeros
+ * @s: string to cut
+ *
+ * Return: length of s
+ */
+int cut_zeros(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	i--;
+	s--;
+	while (*s == '0' && i > 0)
+	{
+		*s = '\0';
+		s--;
+		i--;
+	}
+	return (i);
+}
+
+/**
+ * main - multiple two numbers and print the result
+ * @argc: Number of arguments
+ * @argv: Argument strings
+ *
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+	int *calc;
+	char *final;
+	unsigned int l1, l2, lsum, i, j, ntmp, rolltmp;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
+		_prt("Error"), exit(98);
+	if (_notdigit(argv[1]) || _notdigit(argv[2]))
+		_prt("Error"), exit(98);
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	lsum = l1 + l2, final = malloc((lsum + 2) * sizeof(*final));
+	calc = _calloc(lsum * lsum, sizeof(int));
+	if (calc == NULL)
+		_prt("Error"), exit(98);
+	rev_(argv[1]), rev_(argv[2]);
+	for (i = 0; i < l1; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		rolltmp = 0, ntmp = 0;
+		for (j = 0; j < l2; j++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			ntmp = (argv[1][i] - '0') * (argv[2][j] - '0') + rolltmp;
+			calc[i * lsum + j + i] = ntmp % 10, rolltmp = ntmp / 10;
 		}
-
+		for (; j < l2 + i; j++, rolltmp /= 10)
+			calc[i * lsum + j + i] = rolltmp % 10;
+		while (rolltmp)
+			calc[i * lsum + j + i] = rolltmp % 10, rolltmp /= 10, j++;
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
-}
-
-/**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
- */
-void _puts(char *str)
-{
-  int i = 0;
-  while (str[i])
-    {
-      _putchar(str[i]);
-      i++;
-    }
-}
-/**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
- */
-int _atoi(const char *s)
-{
-  int sign = 1;
-  unsigned long int resp = 0, firstNum, i;
-  for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-    {
-      if (s[firstNum] == '-')
-	{
-	  sign *= -1;
-	}
-    }
-  for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-    {
-      resp *= 10;
-      resp += (s[i] - 48);
-    }
-  return (sign *resp);
-}
-/**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
- */
-void print_int(unsigned long int n)
-{
-  unsigned  long int divisor = 1, i, resp;
-  for (i = 0; n / divisor > 9; i++, divisor *= 10)
-    ;
-  for (; divisor >= 1; n %= divisor, divisor /= 10)
-    {
-      resp = n / divisor;
-      _putchar('0' + resp);
-    }
-}
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
- */
-int main(int argc, char const *argv[])
-{
-  (void)argc;
-  if (argc != 3)
-    {
-      _puts("Error ");
-      exit(98);
-    }
-  print_int(_atoi(argv[1]) * _atoi(argv[2]));
-  _putchar('\n');
-  return (0);
+	for (i = 0, rolltmp = 0; i < lsum; i++, rolltmp /= 10)
+		rolltmp += _addup(calc, lsum, i), final[i] = rolltmp % 10 + '0';
+	final[i + 1] = '\0', i = cut_zeros(final), rev_(final);
+	final[i + 2] = '\0', _prt(final), free(calc), free(final);
+	return (0);
 }
