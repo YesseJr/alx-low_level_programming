80883c4fe37903a4b7e035c4e76c38490df4a8b2
Waython Yesse <thonnieyesse@hotmail.com>
2021-10-14 19:44:53 +0300

    Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,37 +7,32 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	int len = 0, i;
-	unsigned int sum = 0;
+	if (b == '\0')
+		return (0);
 
-	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
+	for (len = 0; b[len];)
 		len++;
-	len -= 1;
 
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	for (len -= 1; len >= 0; len--)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
-	return (sum);
+	return (num);
 }
