16a80a245ecbc8d023be4374f6ae17fa76f6bf69
Waython Yesse <thonnieyesse@hotmail.com>
2021-10-14 12:54:11 +0300

    A function that sets the value of a bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+/*
+ * File: 3-set_bit.c
+ * Author: Waython Yesse
+ * Year: 2021 October 14 (Nyerere Day!)
+ */
+
+#include "main.h"
+
+/**
+ * set_bit - set bit to 1 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max_bits;
+	unsigned long int mask = 1;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* create mask with 1 at index (...00100...) to work on that index */
+	mask <<= index;
+	*n = (*n | mask);
+
+	return (1);
+}
