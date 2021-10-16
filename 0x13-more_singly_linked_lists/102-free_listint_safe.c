f239ae0995215b6b9d168f56ceeb4cb5588b02b7
Waython Yesse <thonnieyesse@hotmail.com>
2021-10-16 07:56:26 +0300

    Uh-huh

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
  * free_listint_safe - frees lists with a loop
  * @h: pointer to a pointer to a listint_t list
  *
  * Return: size of the list that was free'd
  */
+
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *tmp;
