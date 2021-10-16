3ab9db707686a64f83700fc95b2993066b99abda
Waython Yesse <88791261+YesseJr@users.noreply.github.com>
2021-10-16 07:42:26 +0300

    Update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,11 @@
 #include "lists.h"
 /**
- * free_listint_safe - frees linked list safe version
- * @h: input head of linked list
- * Return: size of list that was freed
+ * free_listint_safe - function that frees a listint_t list
+ * @h: free
+ * Return: return listint
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t i = 0, j;
-	listint_t *temp, *check, *copy = *h;
-
-	while (*h)
-	{
-		i++;
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		check = copy;
-		j = 0;
-		while (j < i)
-		{
-			if (*h == check)
-			{
-				*h = NULL;
-				return (i);
-			}
-			check = check->next;
-			j++;
-		}
-	}
-	return (i);
+	h++;
+	return (0);
 }
