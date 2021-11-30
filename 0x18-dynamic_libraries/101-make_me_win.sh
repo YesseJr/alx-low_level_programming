ed94199ddcfde4ccd697742ef6f6d495d5349ff7
YesseJr <thonnieyesse@hotmail.com>
2021-11-30 06:01:15 +0300

    Make me Wiiiiiin

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/YesseJr/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD=/tmp/libwinner.s
+wget -q -O /tmp/gigalib.so "https://github.com/YesseJr/alx-low_level_programming/master/0x18-dynamic_libraries/libwin.so"
+export LD_PRELOAD=/tmp/gigalib.so
