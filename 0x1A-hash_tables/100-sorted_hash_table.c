bc33cb672fdcdc47f6496532a4b0fbe8ddcf702a
YesseJr <thonnieyesse@hotmail.com>
2021-12-21 05:34:47 +0300

    Updated

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 
-shash_table_t *shash_table_create(unsigned long int size);
+shash_table_t *hash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
