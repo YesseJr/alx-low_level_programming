97503eecb3f358e7cf9bd11c90c38ba88a01978c
YesseJr <thonnieyesse@hotmail.com>
2022-02-09 20:00:52 +0300

    My Cellie

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-Module Island Perimeter
+A function that returns the perimeter of the island described in grid:
 """
 
 
 def island_perimeter(grid):
-    """ Calculate perimeter of grid where "1" is found"""
+    """
+    Calculate perimeter of grid where "1" is found
+    """
+    
     cellie = 0
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    cellie += 1  # top
+                    cellie += 1
                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    cellie += 1  # bottom
+                    cellie += 1
                 if col == 0 or grid[row][col - 1] == 0:
-                    cellie += 1  # left
+                    cellie += 1
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    cellie += 1  # right
+                    cellie += 1
     return cellie
