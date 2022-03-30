5cde835ebe73ef2c653a5dd5e837e3bc583e29e9
YesseJr <thonnieyesse@hotmail.com>
2022-03-30 19:56:21 +0300

    Jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,77 +3,48 @@
  */
 
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * print_range - helper func to print at each check
- * @i: index before jump
- * @j: index after jump
+ * jump_search - Performs jump search on a sorted array of integers.
+ *
+ * @array: Pointer to the sorted array of integers.
+ * @size: Size of the sorted array.
+ * @value: Integer value for search for in the array.
+ *
+ * Return: The index at which `value` is found. -1 if the array is NULL or
+ * `value` was not found.
  */
-
-void print_range(size_t i, size_t j)
-{
-	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
-}
-
-/**
- * print_i - helper func to print idx each check
- * @array: array
- * @i: index checked
- */
-
-void print_i(int *array, size_t i)
-{
-	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-}
-
-/**
- * jump_search - find square root, jump steps to find match, end linear search
- * @array: given array of ints
- * @size: size of array
- * @value: value to search for
- * Return: index at which value's found
- */
-
+ 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jumps = sqrt(size);
-	size_t i = 0;
-	size_t j = 0;
+	size_t sqr, lo, hi;
 
-	if (!array)
+	if (array == NULL || size < 1)
 		return (-1);
-	while (j <= size)
-	{
-		if (j != 0)
-			print_i(array, i);
-		if (array[j] >= value)
-		{
-			print_range(i, j);
-			while (i <= j)
-			{
-				print_i(array, i);
-				if (array[i] == value)
-					return (i);
-				i++;
-			}
-			return (-1);
-		}
-		if (j + jumps + jumps > size)
-		{
-			print_i(array, j);
-			print_range(j, j + jumps);
-			while (j < size)
-			{
-				print_i(array, j);
-				if (array[j] == value)
-					return (j);
-				j++;
-			}
-			return (-1);
 
-		}
-		i = j;
-		j += jumps;
+	lo = 0;
+	sqr = sqrt(size);
+	hi = sqr;
+
+	while (1)
+	{
+		printf("Value checked array[%zd] = [%d]\n", lo, array[lo]);
+		if (hi >= size || array[hi] >= value)
+			break;
+		lo = hi;
+		hi += sqr;
+
 	}
+
+	printf("Value found between indexes [%zd] and [%zd]\n", lo, hi);
+
+	for (; lo < hi + 1 && lo < size; lo++)
+	{
+		printf("Value checked array[%zd] = [%d]\n", lo, array[lo]);
+		if (array[lo] == value)
+			return (lo);
+	}
+
 	return (-1);
 }
